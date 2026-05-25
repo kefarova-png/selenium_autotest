cc452e3d6173e80d5894f76a7c8a262913a7dce7
kefarova-png <kefarova@gmail.com>
2026-05-25 23:58:47 +0400

    Версия 2 (headless режим)

--- selenium_autotest.py
+++ selenium_autotest.py
@@ -11,6 +11,8 @@ options = webdriver.ChromeOptions()
 #  Пишем в опции: detach, True -- чтобы Chrome не закрывал окно браузера после завершения работы кода
 options.add_experimental_option("detach",True)
 
+#  Включаем Headless-режим -- без открытия браузера
+options.add_argument("--headless")
 
 #  Создаём вебдрайвер Chrome, с автоматической проверкой/установкой драйвера и c настройками, которые в options
 driver = webdriver.Chrome(
@@ -18,39 +20,39 @@ driver = webdriver.Chrome(
     options=options
 )
 #  Открываем вебдрайвером ссылку
-driver.get('https://app.imyx.ru/auth')
-#  Устанавливаем размер окна
-driver.set_window_size(1920,1080)
+driver.get('https://saucedemo.com/')
+# #  Устанавливаем размер окна
+# driver.set_window_size(1920,1080)
 
-#  Находим элемент для логина, используя XPATH, и Вводим в поле логин "not_office@mail.ru"
-driver.find_element(By.XPATH, "/html/body/div[1]/div/main/div/div/form/div[1]/input").send_keys("not_office@mail.ru")
+#  Находим элемент для логина, используя XPATH, и Вводим в поле логин "standard_user"
+driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[1]/div/div/form/div[1]/input").send_keys("standard_user")
 print('Login input')
 
-#  Находим элемент для пароля, используя XPATH, и Вводим в поле пароль "Shur_1234"
-driver.find_element(By.XPATH, "/html/body/div[1]/div/main/div/div/form/div[2]/div/input").send_keys("Shur_1234")
+#  Находим элемент для пароля, используя XPATH, и Вводим в поле пароль "secret_sauce"
+driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[1]/div/div/form/div[2]/input").send_keys("secret_sauce")
 print('Password input')
 
 #  Найдём и нажмём кнопку входа
-driver.find_element(By.XPATH, "/html/body/div[1]/div/main/div/div/form/button").click()
+driver.find_element(By.ID, "login-button").click()
 print('Login button click')
 
-# Выведем текущий URL и сравним его с ожидаемым
-time.sleep(10) #  Пауза для загрузки стриницы после нажатия кнопки входа
+#  Выведем текущий URL и сравним его с ожидаемым
 get_url = driver.current_url
 print(get_url)
-expected_url = 'https://app.imyx.ru/'
+time.sleep(10)
+expected_url = 'https://www.saucedemo.com/inventory.html'
 assert expected_url == get_url, "Фактический URL должен совпадать с ожидаемым."
 print("The current URL is correct")
 
-#  Найдём заголовок "Генерация" и сравним его с ожидаемым
-get_generation_text = driver.find_element(By.XPATH, '//h1[@class="page-title"]')
+#  Найдём заголовок "Products" и сравним его с ожидаемым
+get_generation_text = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/span')
 get_generation_text_value = get_generation_text.text
 print(f'"{get_generation_text_value}" is the found title')
-expected_generation_text = "Генерация"
+expected_generation_text = "Products"
 assert expected_generation_text == get_generation_text_value, "Фактический заголовок должен совпадать с ожидаемым."
 print("The current page-title is correct")
 
-# Пауза для визуальной проверки
-time.sleep(10)
-# Закрываем браузер
+# #  Пауза для визуальной проверки
+# time.sleep(10)
+#  Закрываем браузер
 driver.close()
